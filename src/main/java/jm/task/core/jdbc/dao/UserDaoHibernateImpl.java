dcaa043dcea0740f01532607ba6148cdf3e030b8
Sir White Mice <il.a.tkachev.personal@gmail.com>
2023-04-04 00:17:40 +0300

    1.2 Работает, но не проходит тестики:С

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,7 +3,7 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
-
+// Мы тут ничего не делаем.
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
 
